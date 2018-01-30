6ba368e35e4b9763e73bbffea247ac7842c79b42
Angel Pescador Portas <35772899+gen8bit@users.noreply.github.com>
2018-01-30 09:56:13 +0100

    Version_1.3

--- BackupVMsrshares01.ps1
+++ BackupVMsrshares01.ps1
@@ -1,11 +1,10 @@
 #
 Write-Host "===================================="
 Write-Host "Backup VMs for Hyper-V"
-Write-Host "(c) Previlabor 2017"
 Write-Host "Author: Angel Pescador Portas"
-Write-Host "email: apescador@previlabor.com"
+Write-Host "email: angel.pescador@gmail.com"
 Write-Host "email: tic@previlabor.com"
-Write-Host "scripts name: BackupVMsrshares01.ps1"
+Write-Host "scripts name: BackupVMservereName3.ps1"
 Write-Host "Version 1.3"
 Write-Host "Date: 29/12/2017"
 Write-Host "===================================="
@@ -19,17 +18,18 @@ $StartBCK = Get-Date -UFormat "%H-%M"
 
 
 # VM, Source & Destination Folders
-# For ALL, Use: Get-VM | foreach Name
-$VM = 'srshares01'
+# For ALL, Use: Get-VM | foreach Name
+
+$VM = 'ServerName3'
 $destination = 'K:\BackupVMs\'+$VM +'\' +$Bckdate
 
 TITLE Export VM $VM in $destination
 
 # eMail Setting
-$From = 'SRVM01@previlabor.com'
-$To = 'apescador@previlabor.com'
+$From = 'YourServerName@domain.com'
+$To = 'YourEmail@domain.com'
 $Subject = 'END of Export VM ' + $VM
-$smtpServer = 'previlabor-com.mail.protection.outlook.com'
+$smtpServer = 'YourSMTPServer'
 
 
 # keep only 2 backup of this VM
@@ -76,14 +76,16 @@ Write-Host "======================================================"
 Write-Host
 
 
-C:\Scripts\BackUpVMs\7za.exe a $destination\$VM-7ZA.7z $destination -r -V1G -bt -y -mx=9 -ms=on -xr@C:\Scripts\BackUpVMs\exclude.txt -mhe -sdel
+7za.exe a $destination\$VM-7ZA.7z $destination -r -V1G -bt -y -mx=9 -ms=on -xr@exclude.txt -mhe -sdel
 
 $ENDBCK = Get-Date -UFormat "%H-%M"
 $Body = 'END of Backup ' + $VM + ' | ' + ' Start process ' + $StartBCK + ' | ' + ' END process ' + $ENDBCK
 
 send-mailmessage -from $From -to $To -Subject $Subject -body $Body -smtpServer $smtpServer
 
-Write-HostWrite-Host " END Process " $ENDBCK hWrite-Host " --------------------- "
+Write-Host
+Write-Host " END Process " $ENDBCK h
+Write-Host " --------------------- "
 Write-Host
 
 
