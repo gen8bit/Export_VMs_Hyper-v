d41b7d738b66de64682fa9e32cd23434850f099e
Angel Pescador Portas <35772899+gen8bit@users.noreply.github.com>
2018-01-30 09:51:36 +0100

    Version_1.3

--- BackupVMsrdc1.ps1
+++ BackupVMsrdc1.ps1
@@ -1,10 +1,8 @@
 #
 Write-Host "===================================="
 Write-Host "Backup VMs for Hyper-V"
-Write-Host "(c) Previlabor 2017"
 Write-Host "Author: Angel Pescador Portas"
 Write-Host "email: apescador@previlabor.com"
-Write-Host "email: tic@previlabor.com"
 Write-Host "scripts name: BackupVMsrsdc1.ps1"
 Write-Host "Version 1.3"
 Write-Host "Date: 29/12/2017"
@@ -19,16 +17,17 @@ $StartBCK = Get-Date -UFormat "%H-%M"
 
 
 # VM, Source & Destination Folders
-# For ALL, Use: Get-VM | foreach Name
-$VM = 'srdc1'
+# For ALL, Use: Get-VM | foreach Name
+
+$VM = 'SERVERNAMETOEXPORT'
 $destination = 'K:\BackupVMs\'+$VM +'\' +$Bckdate
 
 
 # eMail Setting
-$From = 'SRVM01@previlabor.com'
-$To = 'apescador@previlabor.com'
+$From = 'NameOfYourServer@domain.com'
+$To = 'youremail@domain.com'
 $Subject = 'END of Export VM ' + $VM
-$smtpServer = 'previlabor-com.mail.protection.outlook.com'
+$smtpServer = 'yourSMTPServer'
 
 
 # keep only 15 backup of this VM
@@ -75,14 +74,16 @@ Write-Host "======================================================"
 Write-Host
 
 
-C:\Scripts\BackUpVMs\7za.exe a $destination\$VM-7ZA.7z $destination -r -V1G -bt -y -mx=9 -ms=on -xr@C:\Scripts\BackUpVMs\exclude.txt -mhe -sdel
+.\7za.exe a $destination\$VM-7ZA.7z $destination -r -V1G -bt -y -mx=9 -ms=on -xr@exclude.txt -mhe -sdel
 
 $ENDBCK = Get-Date -UFormat "%H-%M"
 $Body = 'END of Backup ' + $VM + ' | ' + ' Start process ' + $StartBCK + ' | ' + ' END process ' + $ENDBCK
 
 send-mailmessage -from $From -to $To -Subject $Subject -body $Body -smtpServer $smtpServer
 
-Write-HostWrite-Host " END Process " $ENDBCK hWrite-Host " --------------------- "
+Write-Host
+Write-Host " END Process " $ENDBCK h
+Write-Host " --------------------- "
 Write-Host
 
 
